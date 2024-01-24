e21e4068542c03899fa79f55a3fa8d415559c2ec
JackPadalino <pada0867@gmail.com>
2024-01-24 13:42:41 -0500

    installed react-player library - now playing video in Videos component - need to test with multiple videos - having issue being able to navigate throughout video with controls

--- src/components/Videos/Videos.jsx
+++ src/components/Videos/Videos.jsx
@@ -1,26 +1,54 @@
 import { useState, useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
+import React from "react";
+import ReactPlayer from "react-player";
+
 import "./videos.css";
 
 const Videos = () => {
   const { storeVideos } = useSelector((state) => state.music);
+  const [renderPlayer, setRenderPlayer] = useState(false);
+  const [playingVideo, setPlayingVideo] = useState("");
 
-  console.log(storeVideos);
+  const handlePlay = (video) => {
+    setRenderPlayer(true);
+    setPlayingVideo(video);
+  };
 
   return (
-    <>
-      <h1>This is the Videos component</h1>
-      {storeVideos.map((video) => (
-        <div key={video.id}>
-          <h3>
-            {video.title} - {video.upload_date}
-          </h3>
-          {video.tags.map((tag) => (
-            <p key={tag.id}>#{tag.title}</p>
-          ))}
+    <div style={{ display: "flex", gap: "50px" }}>
+      <div>
+        <h1>This is the Videos component</h1>
+        {storeVideos.map((video) => (
+          <div key={video.id}>
+            <h3 onClick={() => handlePlay(video)} style={{ cursor: "pointer" }}>
+              {video.title} - {video.upload_date}
+            </h3>
+            <div style={{ display: "flex", gap: "10px" }}>
+              {video.tags.map((tag) => (
+                <p key={tag.id}>#{tag.title}</p>
+              ))}
+            </div>
+          </div>
+        ))}
+      </div>
+      {renderPlayer && (
+        <div>
+          <ReactPlayer
+            url={`http://localhost:8000${playingVideo.file}`}
+            // url="https://www.youtube.com/watch?v=HkKr8HG3qUA"
+            controls={true}
+            muted={false}
+            volume={1}
+          />
+          <div style={{ display: "flex", gap: "10px" }}>
+            {playingVideo.tags.map((tag) => (
+              <p key={tag.id}>#{tag.title}</p>
+            ))}
+          </div>
         </div>
-      ))}
-    </>
+      )}
+    </div>
   );
 };
 
