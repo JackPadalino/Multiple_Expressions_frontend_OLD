55e158e2a68d96a4aebd3628e7281fca08afc79f
JackPadalino <pada0867@gmail.com>
2024-02-01 14:03:03 -0500

    added live stream link

--- src/components/Live/Live.jsx
+++ src/components/Live/Live.jsx
@@ -1,9 +1,11 @@
 import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 
 const Live = () => {
   return (
     <>
       <h1>Live</h1>
+      <Link to={`${import.meta.env.VITE_LIVE_STREAM_LINK}`}>Join live</Link>
     </>
   );
 };
