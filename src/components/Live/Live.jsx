2e1164e6aeac548d555063d06aa74d81718cf044
JackPadalino <pada0867@gmail.com>
2024-02-03 13:48:39 -0500

    added user authetication with JWT to Live component - conditionally rendering player and login form

--- src/components/Live/Live.jsx
+++ src/components/Live/Live.jsx
@@ -1,11 +1,116 @@
 import { useState, useEffect, useRef } from "react";
 import { Link } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import axios from "axios";
 import ReactPlayer from "react-player";
+import VideoJS from "../VideoJS/VideoJS";
+import {
+  Box,
+  Container,
+  Typography,
+  TextField,
+  Button,
+  Avatar,
+} from "@mui/material";
 import "./live.css";
 
-import VideoJS from "../VideoJS/VideoJS";
-
 const Live = () => {
+  // user login
+  const [credentials, setCredentials] = useState({
+    username: "",
+    password: "",
+  });
+  const [isAuthenicated, setIsAuthenticated] = useState(false);
+
+  const onCredentialsChange = (e) => {
+    setCredentials({ ...credentials, [e.target.name]: e.target.value });
+  };
+
+  const handleLogin = async (event) => {
+    event.preventDefault();
+    try {
+      let url;
+      if (import.meta.env.VITE_DEV_MODE === "true") {
+        url = import.meta.env.VITE_DEV_URL;
+      } else {
+        url = import.meta.env.VITE_PROD_URL;
+      }
+
+      const response = await axios.post(`${url}/api/users/token`, credentials);
+      if (response.status === 200 || response.status === 204) {
+        window.localStorage.clear();
+        window.localStorage.setItem("access_token", response.data.access);
+        window.localStorage.setItem("refresh_token", response.data.refresh);
+        setIsAuthenticated(true);
+      } else {
+        console.error(`Unexpected response status: ${response.status}`);
+        setIsAuthenticated(false);
+      }
+    } catch (error) {
+      // Handle error
+      if (error.response) {
+        console.error(
+          "Request failed with status code:",
+          error.response.status
+        );
+      } else if (error.request) {
+        console.error("No response received:", error.request);
+      } else {
+        console.error("Error setting up the request:", error.message);
+      }
+
+      // setIsAuthenticated(false);
+    }
+  };
+
+  const handleLogout = async () => {
+    try {
+      let url;
+      if (import.meta.env.VITE_DEV_MODE === "true") {
+        url = import.meta.env.VITE_DEV_URL;
+      } else {
+        url = import.meta.env.VITE_PROD_URL;
+      }
+
+      const token = window.localStorage.getItem("refresh_token");
+
+      const response = await axios.post(`${url}/api/users/logout`, {
+        refresh_token: token,
+      });
+
+      if (response.status === 200 || response.status === 204) {
+        window.localStorage.clear();
+        setIsAuthenticated(false);
+      } else {
+        console.error(`Unexpected response status: ${response.status}`);
+        setIsAuthenticated(false);
+      }
+    } catch (error) {
+      if (error.response) {
+        console.error(
+          "Request failed with status code:",
+          error.response.status
+        );
+      } else if (error.request) {
+        console.error("No response received:", error.request);
+      } else {
+        console.error("Error setting up the request:", error.message);
+      }
+      // setIsAuthenticated(false);
+    }
+  };
+
+  useEffect(() => {
+    // check for the presence of a token in localStorage
+    const token = localStorage.getItem("access_token");
+
+    if (token) {
+      setIsAuthenticated(true);
+    } else {
+      setIsAuthenticated(false);
+    }
+  }, []);
+
   // videoJS
   const playerRef = useRef(null);
 
@@ -58,7 +163,53 @@ const Live = () => {
           />
         </div>
       </div> */}
-      <VideoJS options={videoJsOptions} onReady={handlePlayerReady} />
+      {!isAuthenicated && (
+        <>
+          <VideoJS options={videoJsOptions} onReady={handlePlayerReady} />
+          <Container component="main" maxWidth="xs">
+            <Box
+              className="mainBox"
+              component="form"
+              noValidate
+              autoComplete="off"
+              onSubmit={handleLogin}
+            >
+              <TextField
+                //margin="normal"
+                required
+                fullWidth
+                onChange={onCredentialsChange}
+                id="username"
+                label="Username"
+                name="username"
+                //autoComplete="username"
+                value={credentials.username}
+                //autoFocus
+              />
+              <TextField
+                //margin="normal"
+                required
+                fullWidth
+                onChange={onCredentialsChange}
+                name="password"
+                label="Password"
+                type="password"
+                id="password"
+                value={credentials.password}
+                //autoComplete="current-password"
+              />
+              <Button type="submit" fullWidth variant="contained" size="large">
+                Login
+              </Button>
+            </Box>
+          </Container>
+        </>
+      )}
+      {isAuthenicated && (
+        <Button onClick={() => handleLogout()} variant="contained" size="large">
+          Logout
+        </Button>
+      )}
     </>
   );
 };
