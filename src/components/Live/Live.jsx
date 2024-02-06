5ed18062273f6d25fc90ba269d2febdf2d21c3c4
JackPadalino <pada0867@gmail.com>
2024-02-05 19:59:03 -0500

    updated styling of player and added event listeners and messages to live component

--- src/components/Live/Live.jsx
+++ src/components/Live/Live.jsx
@@ -12,16 +12,31 @@ import "./live.css";
 
 const Live = () => {
   const videoPlayerRef = useRef(null);
+  const [isPlaying, setIsPlaying] = useState(false);
+  const [hasEnded, setHasEnded] = useState(false);
 
   useEffect(() => {
-    const videoPlayer = videoPlayerRef.current;
-
     // check if IVSPlayer is supported by the browser
-    if (IVSPlayer.isPlayerSupported && videoPlayer) {
+    if (IVSPlayer.isPlayerSupported && videoPlayerRef.current) {
       const player = IVSPlayer.create();
-      player.attachHTMLVideoElement(videoPlayer);
+      player.attachHTMLVideoElement(videoPlayerRef.current);
       player.load(import.meta.env.VITE_LIVE_STREAM_LINK);
       player.play();
+
+      //listen for player events
+      player.addEventListener(IVSPlayer.PlayerState.PLAYING, () => {
+        setIsPlaying(true);
+      });
+
+      player.addEventListener(IVSPlayer.PlayerState.ENDED, () => {
+        setHasEnded(true);
+        setIsPlaying(false);
+      });
+
+      // listen for errors (use to auto start)
+      player.addEventListener(IVSPlayer.PlayerEventType.ERROR, (err) => {
+        if (err.type === "ErrorNotAvailable") setIsPlaying(false);
+      });
 
       // clean up the player when the component unmounts
       return () => {
@@ -32,10 +47,34 @@ const Live = () => {
 
   return (
     <>
-      <Typography variant="h4">Join us live!</Typography>
-      <Box>
-        <video ref={videoPlayerRef} id="video-player" playsInline></video>
+      {!isPlaying && !hasEnded && (
+        <Typography variant="h6" sx={{ textAlign: "center" }}>
+          We are not live right now. Check back soon!
+        </Typography>
+      )}
+      {isPlaying && (
+        <Typography variant="h6" sx={{ textAlign: "center" }}>
+          Live
+        </Typography>
+      )}
+      {hasEnded && (
+        <Typography variant="h6" sx={{ textAlign: "center" }}>
+          Our live stream has ended. Thanks for coming!
+        </Typography>
+      )}
+      <Box className="playerContainer">
+        <video
+          ref={videoPlayerRef}
+          className="player"
+          id="video-player"
+          playsInline
+          controls
+        ></video>
       </Box>
+      <Typography variant="h6" sx={{ textAlign: "center" }}>
+        (If video <span style={{ fontStyle: "italic" }}>should</span> be playing
+        try refreshing the page.)
+      </Typography>
     </>
   );
 };
