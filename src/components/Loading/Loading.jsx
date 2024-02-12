9740bf6d999ad9c031e1bb243bbb708f5bb54d68
JackPadalino <pada0867@gmail.com>
2024-02-12 14:30:14 -0500

    removed images from NotFound and Loading components - updated css

--- src/components/Loading/Loading.jsx
+++ src/components/Loading/Loading.jsx
@@ -3,12 +3,6 @@ import "./loading.css";
 const Loading = () => {
   return (
     <div className="loadingMainContainer">
-      <img
-        src={`https://${
-          import.meta.env.VITE_AWS_S3_BUCKET
-        }.s3.amazonaws.com/site_photos/logo.jpeg`}
-        className="loadingImg"
-      />
       <h2 className="loadingTitle">Patience...</h2>
     </div>
   );
--- src/components/NotFound/NotFound.jsx
+++ src/components/NotFound/NotFound.jsx
@@ -4,14 +4,6 @@ import "./notFound.css";
 const NotFound = () => {
   return (
     <div className="notFoundMainContainer">
-      <Link to="/auditory">
-        <img
-          src={`https://${
-            import.meta.env.VITE_AWS_S3_BUCKET
-          }.s3.amazonaws.com/site_photos/logo.jpeg`}
-          className="notFoundImg"
-        />
-      </Link>
       <h2 className="notFoundTitle">Sorry. Page not found.</h2>
     </div>
   );
