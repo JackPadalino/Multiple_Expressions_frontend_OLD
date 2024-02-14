f4eec4dba5d14a618af3bda8e143eb0c4e1d5a15
JackPadalino <pada0867@gmail.com>
2024-02-14 12:11:47 -0500

    have home slice images fading in sequentially, need to turn into a link now

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -51,102 +51,168 @@
 
 // export default Home;
 
-import React, { useState, useEffect, useRef } from "react";
+// import React, { useState, useEffect, useRef } from "react";
+// import { Link } from "react-router-dom";
+// import "./home.css";
+
+// const Home = () => {
+//   const imgRef1 = useRef(null);
+//   const imgRef2 = useRef(null);
+//   const imgRef3 = useRef(null);
+//   const imgRef4 = useRef(null);
+
+//   // using useEffect to create the fade-in animation when component loads
+//   useEffect(() => {
+//     const handleImageLoad = (imgRef) => {
+//       imgRef.current.classList.add("imgLoaded");
+//     };
+
+//     if (imgRef1.current) {
+//       imgRef1.current.addEventListener("load", () => handleImageLoad(imgRef1));
+//     }
+
+//     if (imgRef2.current) {
+//       imgRef2.current.addEventListener("load", () => handleImageLoad(imgRef2));
+//     }
+
+//     if (imgRef3.current) {
+//       imgRef3.current.addEventListener("load", () => handleImageLoad(imgRef3));
+//     }
+//     if (imgRef4.current) {
+//       imgRef4.current.addEventListener("load", () => handleImageLoad(imgRef4));
+//     }
+
+//     return () => {
+//       if (imgRef1.current) {
+//         imgRef1.current.removeEventListener("load", () =>
+//           handleImageLoad(imgRef1)
+//         );
+//       }
+//       if (imgRef2.current) {
+//         imgRef2.current.removeEventListener("load", () =>
+//           handleImageLoad(imgRef2)
+//         );
+//       }
+//       if (imgRef3.current) {
+//         imgRef3.current.removeEventListener("load", () =>
+//           handleImageLoad(imgRef3)
+//         );
+//       }
+//       if (imgRef4.current) {
+//         imgRef4.current.removeEventListener("load", () =>
+//           handleImageLoad(imgRef4)
+//         );
+//       }
+//     };
+//   }, []);
+
+//   return (
+//     <div className="homeMainContainer">
+//       <img
+//         src={`https://${
+//           import.meta.env.VITE_AWS_S3_BUCKET
+//         }.s3.amazonaws.com/site_photos/Home Img Slice 1.jpg`}
+//         ref={imgRef1}
+//         id="homeImg1"
+//         className="homeImg"
+//         alt="Home Image"
+//       />
+//       <img
+//         src={`https://${
+//           import.meta.env.VITE_AWS_S3_BUCKET
+//         }.s3.amazonaws.com/site_photos/Home Img Slice 2.jpg`}
+//         ref={imgRef2}
+//         id="homeImg2"
+//         className="homeImg"
+//         alt="Home Image"
+//       />
+//       <img
+//         src={`https://${
+//           import.meta.env.VITE_AWS_S3_BUCKET
+//         }.s3.amazonaws.com/site_photos/Home Img Slice 3.jpg`}
+//         ref={imgRef3}
+//         id="homeImg3"
+//         className="homeImg"
+//         alt="Home Image"
+//       />
+//       <img
+//         src={`https://${
+//           import.meta.env.VITE_AWS_S3_BUCKET
+//         }.s3.amazonaws.com/site_photos/Home Img Slice 4.jpg`}
+//         ref={imgRef4}
+//         id="homeImg4"
+//         className="homeImg"
+//         alt="Home Image"
+//       />
+//       <h2 className="homeTitle">Multiple Expressions</h2>
+//     </div>
+//   );
+// };
+
+// export default Home;
+
+import React, { useEffect, useRef } from "react";
 import { Link } from "react-router-dom";
 import "./home.css";
 
 const Home = () => {
-  const imgRef1 = useRef(null);
-  const imgRef2 = useRef(null);
-  const imgRef3 = useRef(null);
-  const imgRef4 = useRef(null);
+  const imgRefs = [useRef(null), useRef(null), useRef(null), useRef(null)];
 
-  // using useEffect to create the fade-in animation when component loads
+  const loadAndAnimateImage = (index) => {
+    if (index < imgRefs.length) {
+      const currentImgRef = imgRefs[index];
+      const nextIndex = index + 1;
+
+      if (currentImgRef.current) {
+        currentImgRef.current.src = currentImgRef.current.dataset.src;
+        currentImgRef.current.addEventListener("load", () => {
+          currentImgRef.current.classList.add("imgLoaded");
+          setTimeout(() => {
+            loadAndAnimateImage(nextIndex);
+          }, 1000); // Adjust the delay as needed
+        });
+      }
+    }
+  };
+
   useEffect(() => {
-    const handleImageLoad = (imgRef) => {
-      imgRef.current.classList.add("imgLoaded");
-    };
-
-    if (imgRef1.current) {
-      imgRef1.current.addEventListener("load", () => handleImageLoad(imgRef1));
-    }
-
-    if (imgRef2.current) {
-      imgRef2.current.addEventListener("load", () => handleImageLoad(imgRef2));
-    }
-
-    if (imgRef3.current) {
-      imgRef3.current.addEventListener("load", () => handleImageLoad(imgRef3));
-    }
-    if (imgRef4.current) {
-      imgRef4.current.addEventListener("load", () => handleImageLoad(imgRef4));
-    }
-
-    return () => {
-      if (imgRef1.current) {
-        imgRef1.current.removeEventListener("load", () =>
-          handleImageLoad(imgRef1)
-        );
-      }
-      if (imgRef2.current) {
-        imgRef2.current.removeEventListener("load", () =>
-          handleImageLoad(imgRef2)
-        );
-      }
-      if (imgRef3.current) {
-        imgRef3.current.removeEventListener("load", () =>
-          handleImageLoad(imgRef3)
-        );
-      }
-      if (imgRef4.current) {
-        imgRef4.current.removeEventListener("load", () =>
-          handleImageLoad(imgRef4)
-        );
-      }
-    };
+    loadAndAnimateImage(0);
   }, []);
 
   return (
     <div className="homeMainContainer">
-      {/* <Link to="/auditory"> */}
       <img
-        src={`https://${
+        data-src={`https://${
           import.meta.env.VITE_AWS_S3_BUCKET
         }.s3.amazonaws.com/site_photos/Home Img Slice 1.jpg`}
-        ref={imgRef1}
-        id="homeImg1"
+        ref={imgRefs[0]}
         className="homeImg"
         alt="Home Image"
       />
       <img
-        src={`https://${
+        data-src={`https://${
           import.meta.env.VITE_AWS_S3_BUCKET
         }.s3.amazonaws.com/site_photos/Home Img Slice 2.jpg`}
-        ref={imgRef2}
-        id="homeImg2"
+        ref={imgRefs[1]}
         className="homeImg"
         alt="Home Image"
       />
       <img
-        src={`https://${
+        data-src={`https://${
           import.meta.env.VITE_AWS_S3_BUCKET
         }.s3.amazonaws.com/site_photos/Home Img Slice 3.jpg`}
-        ref={imgRef3}
-        id="homeImg3"
+        ref={imgRefs[2]}
         className="homeImg"
         alt="Home Image"
       />
       <img
-        src={`https://${
+        data-src={`https://${
           import.meta.env.VITE_AWS_S3_BUCKET
         }.s3.amazonaws.com/site_photos/Home Img Slice 4.jpg`}
-        ref={imgRef4}
-        id="homeImg4"
+        ref={imgRefs[3]}
         className="homeImg"
         alt="Home Image"
       />
-      {/* </Link> */}
-      <h2 className="homeTitle">Multiple Expressions</h2>
     </div>
   );
 };
