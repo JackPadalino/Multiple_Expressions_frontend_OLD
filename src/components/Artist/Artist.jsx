cbcbb92f0f5ea4a72c84af67f3901ef5f9c46e05
JackPadalino <pada0867@gmail.com>
2024-02-15 19:25:15 -0500

    added tracks to artist page

--- src/components/Artist/Artist.jsx
+++ src/components/Artist/Artist.jsx
@@ -1,17 +1,27 @@
 import { useState, useEffect } from "react";
-import { useParams } from "react-router-dom";
+import { useParams, Link } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
 import axios from "axios";
 import { SocialIcon } from "react-social-icons/component";
 import "react-social-icons/instagram";
 import "react-social-icons/soundcloud";
 
+import { Box, Typography, Avatar, IconButton } from "@mui/material";
+import PlayArrowIcon from "@mui/icons-material/PlayArrow";
+import { setWaveformTrack } from "../../store/waveformSlice";
+
 import Loading from "../Loading/Loading";
 import "./artist.css";
 
 const Artist = () => {
+  const dispatch = useDispatch();
   const { id } = useParams();
   const [loading, setLoading] = useState(true);
   const [artist, setArtist] = useState({});
+
+  const handlePlay = (track) => {
+    dispatch(setWaveformTrack(track));
+  };
 
   const fetchArtistData = async () => {
     let url;
@@ -23,6 +33,7 @@ const Artist = () => {
     try {
       const response = await axios.get(`${url}/api/music/artists/${id}`);
       setArtist(response.data);
+      console.log(response.data);
       setLoading(false);
     } catch (error) {
       if (error.response) {
@@ -64,6 +75,50 @@ const Artist = () => {
             </div>
           </div>
           <img className="artistPageDesktopImg" src={artist.profile_photo} />
+        </div>
+        <div className="artistPageTracks">
+          <h1>Tracks</h1>
+          {artist.tracks.map((track) => (
+            <div key={track.id}>
+              <div className="artistPageTrackTitlePlayDiv">
+                <h2
+                  className="artistPageTrackTitle"
+                  onClick={() => handlePlay(track)}
+                >
+                  {track.title}
+                </h2>
+                <IconButton
+                  onClick={() => handlePlay(track)}
+                  sx={{ padding: "0px", margin: "0px" }}
+                >
+                  <PlayArrowIcon
+                    fontSize="medium"
+                    sx={{
+                      color: "orange",
+                    }}
+                  />
+                </IconButton>
+              </div>
+              <div className="artistPageArtistInfoDiv">
+                {track.artists.map((artist) => (
+                  <Link
+                    key={artist.id}
+                    to={`/artist/${artist.id}`}
+                    className="artistPageArtistLink"
+                  >
+                    {artist.name}
+                  </Link>
+                ))}
+              </div>
+              <div className="artistPageTagsDiv">
+                {track.tags.map((tag) => (
+                  <p className="artistPageTag" key={tag.id}>
+                    #{tag.title}
+                  </p>
+                ))}
+              </div>
+            </div>
+          ))}
         </div>
       </div>
     </>
