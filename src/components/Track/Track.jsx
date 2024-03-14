420d52bd0379035fcfc0bac92297d247de622d6e
JackPadalino <pada0867@gmail.com>
2024-03-14 18:54:04 -0400

    began styling Track component - need to add play functionality

--- src/components/Track/Track.jsx
+++ src/components/Track/Track.jsx
@@ -1,8 +1,72 @@
-import React from "react";
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import axios from "axios";
+import { Box, IconButton } from "@mui/material";
+import PlayArrowIcon from "@mui/icons-material/PlayArrow";
+import { setWaveformTrack } from "../../store/waveformSlice";
 import "./track.css";
 
 const Track = () => {
-  return <div></div>;
+  const dispatch = useDispatch();
+  const { id } = useParams();
+  const [track, setTrack] = useState({});
+  const [loading, setLoading] = useState(true);
+
+  const formatDate = (track) => {
+    const dateObject = new Date(track.upload_date);
+    const formattedDate = dateObject.toISOString().split("T")[0];
+    return {
+      ...track,
+      upload_date: formattedDate,
+    };
+  };
+
+  const fetchTrackData = async () => {
+    let url;
+    if (import.meta.env.VITE_DEV_MODE === "true") {
+      url = import.meta.env.VITE_DEV_URL;
+    } else {
+      url = import.meta.env.VITE_PROD_URL;
+    }
+    try {
+      const response = await axios.get(`${url}/api/music/tracks/${id}`);
+      const updatedTrack = formatDate(response.data);
+      setTrack(updatedTrack);
+      setLoading(false);
+    } catch (error) {
+      if (error.response) {
+        console.error(
+          "Request failed with status code:",
+          error.response.status
+        );
+      } else if (error.request) {
+        console.error("No response received:", error.request);
+      } else {
+        console.error("Error setting up the request:", error.message);
+      }
+    }
+  };
+
+  const handlePlay = (track) => {
+    dispatch(setWaveformTrack(track));
+  };
+
+  useEffect(() => {
+    fetchTrackData();
+  }, []);
+
+  if (loading) return null;
+  return (
+    <Box className="trackMainContainer">
+      <img
+        className="trackPhoto"
+        src={track.track_photo}
+        // onClick={() => handlePlay(track)}
+      />
+      <h2 className="trackTitle">{track.title}</h2>
+    </Box>
+  );
 };
 
 export default Track;
